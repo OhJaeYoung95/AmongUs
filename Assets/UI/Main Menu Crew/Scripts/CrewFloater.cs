c7a3b5271a9b2ea24a43d74c4f6ec0568438db15
OhJaeYoung <ostyoung@gmail.com>
2023-02-17 22:36:57 +0900

    Main Menu Part2

--- Assets/UI/Main Menu Crew/Scripts/CrewFloater.cs
+++ Assets/UI/Main Menu Crew/Scripts/CrewFloater.cs
@@ -16,7 +16,11 @@ public class CrewFloater : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        // 카메라안에서부터 크루원 생성
+        for (int i = 0; i < 12; i++)
+        {
+            SpawnFloatingCrew((EPlayerColor)i, Random.Range(0, distance));
+        }   
     }
 
     // Update is called once per frame
@@ -39,22 +43,38 @@ public class CrewFloater : MonoBehaviour
             crewStates[(int)playerColor] = true;
             float angle = Random.Range(0f, 360f);
             Vector3 spawnPos = new Vector3(Mathf.Sin(angle), Mathf.Cos(angle), 0f) * dist;  // 카메라 영역 밖의 원형으로 생성되도록 설정
-            Vector3 direction = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), 0f);
+
+            //Vector3 direction = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), 0f);
+            // 스폰 위치에 따른 랜덤방향
+            Vector3 direction;
+            if (spawnPos.x >= 0 && spawnPos.y >= 0)
+                direction = new Vector3(Random.Range(-1f, 0f), Random.Range(-1f, 0f), 0);
+            else if(spawnPos.x < 0 && spawnPos.y >= 0)
+                direction = new Vector3(Random.Range(0f, 1f), Random.Range(-1f, 0f), 0);
+            else if(spawnPos.x >= 0 && spawnPos.y < 0)
+                direction = new Vector3(Random.Range(-1f, 0f), Random.Range(0f, 1f), 0);
+            else if(spawnPos.x < 0 && spawnPos.y < 0)
+                direction = new Vector3(Random.Range(0f, 1f), Random.Range(0f, 1f), 0);
+            else
+                direction = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f), 0);
             float floatingSpeed = Random.Range(1f, 4f);
             float rotateSpeed = Random.Range(-3f, 3f);
 
             var crew = Instantiate(prefab, spawnPos, Quaternion.identity).GetComponent<FloatingCrew>();
+
             crew.SetFloatingCrew(sprites[Random.Range(0, sprites.Count)], playerColor, direction,
                 floatingSpeed, rotateSpeed, Random.Range(0.5f, 1f));
         }
     }
 
+    // 콜라이더 영역 밖을 나갈때 크루원 제거, 크루원색 초기화
     private void OnTriggerExit2D(Collider2D collision)
     {
         var crew = collision.GetComponent<FloatingCrew>();
         if(crew != null)
         {
-
+            crewStates[(int)crew.playerColor] = false;
+            Destroy(crew.gameObject);
         }
     }
 }
