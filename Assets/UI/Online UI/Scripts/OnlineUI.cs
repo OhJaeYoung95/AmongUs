a39ef30137edabb430bb7799b713aa19bf8c179d
OhJaeYoung <ostyoung@naver.com>
2023-03-12 23:07:30 +0900

    Build

--- Assets/UI/Online UI/Scripts/OnlineUI.cs
+++ Assets/UI/Online UI/Scripts/OnlineUI.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using Mirror;
 
 public class OnlineUI : MonoBehaviour
 {
@@ -24,15 +25,18 @@ public class OnlineUI : MonoBehaviour
             nicknameInputField.GetComponent<Animator>().SetTrigger("on");
         }
     }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+    public void OnClickEnterGameRoomButton()
     {
-        
+        // 닉네임 입력한 경우
+        if (nicknameInputField.text != "")
+        {
+            var manager = AmongUsRoomManager.singleton;
+            manager.StartClient();
+        }
+        else    // 닉네임 입력하지 않은 경우
+        {
+            nicknameInputField.GetComponent<Animator>().SetTrigger("on");
+        }
     }
 }
